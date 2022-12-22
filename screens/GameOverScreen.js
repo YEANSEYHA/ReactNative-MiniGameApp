e3260b75ca84bcf7f63e175003eb75d4e89c566f
YEAN SEHA <yeanseyha7@gmail.com>
2022-12-22 16:08:52 +0700

    ReactNatice:52.Adding Visual Feedback

---
+++ screens/GameOverScreen.js
@@ -0,0 +1,11 @@
+import { TextInput, View } from 'react-native';
+
+
+function StartGameScreen() {
+    return <View>
+        <TextInput />
+        BUTTONS
+    </View>;
+}
+
+export default StartGameScreen;
